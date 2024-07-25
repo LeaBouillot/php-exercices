a129af071486d9d57f9c2fa21a9509de737803d8
bestdevelope <leabouillot7@gmail.com>
2024-07-25 19:49:25 +0200

    create table

--- createdb.php
+++ createdb.php
@@ -1,4 +1,32 @@
-<!-- creation db PDO -->
 <?php
 
-?>
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=firstdb", $username, $password);
+    $conn->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //error msg
+
+    echo "<p>Connected successfully.</p>";
+} catch (PDOException $e) {
+    echo $e->getMessage();
+    exit;
+}
+
+//creation table
+try {
+    $sql = "CREATE TABLE Myuests (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+    $conn->exec($sql);
+    echo "Table MyGuests created successfully";
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+$conn = null;
